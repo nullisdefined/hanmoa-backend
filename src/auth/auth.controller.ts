2ac0dbd53cf98b2ed41721f3a7c60495144cc85a
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-11-14 19:14:43 +0900

    feat: auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { GithubAuthGuard } from './guards/github-auth.guard';
+import { User } from 'src/modules/users/entities/user.entity';
+import { Request } from 'express';
+
+interface RequestWithUser extends Request {
+  user: User;
+}
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('github')
+  @UseGuards(GithubAuthGuard)
+  async githubLogin() {}
+
+  @Get('github/callback')
+  @UseGuards(GithubAuthGuard)
+  githubCallback(@Req() req: RequestWithUser) {
+    return this.authService.login(req.user);
+  }
+}
