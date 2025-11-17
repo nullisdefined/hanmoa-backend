1f66c06081db3d2140d12b82803a1803008abffd
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-11-17 12:54:03 +0900

    refactor: User entity typings

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,20 +1,26 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { Project } from './project.entity';
+
+export type authProvider = 'google' | 'github';
 
 @Entity('user')
 export class User extends BaseEntity {
-  @Column({ type: 'varchar', unique: true })
+  @Column({ unique: true })
   email: string;
 
-  @Column({ type: 'varchar' })
+  @Column()
   nickname: string;
 
-  @Column({ name: 'avatar_url', type: 'varchar' })
+  @Column({ name: 'avatar_url' })
   avatarUrl: string;
 
-  @Column({ name: 'auth_provider', type: 'varchar' })
-  authProvider: string;
+  @Column({ name: 'auth_provider' })
+  authProvider: authProvider;
 
-  @Column({ name: 'provider_user_id', type: 'varchar' })
+  @Column({ name: 'provider_user_id' })
   providerUserId: string;
+
+  @OneToMany(() => Project, (project: Project) => project.user)
+  projects: Project[];
 }
