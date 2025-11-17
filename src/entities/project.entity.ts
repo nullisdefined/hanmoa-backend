4d5c4c2165fe09ea8148b95259e4316ae9634b04
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-11-17 15:28:11 +0900

    feat: add Project entity

---
+++ src/entities/project.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { User } from './user.entity';
+import { VideoAsset } from './video-asset.entity';
+import { DubJob } from './dub-job.entity';
+
+@Entity('project')
+export class Project extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @Column({ name: 'project_id' })
+  projectId: string;
+
+  @ManyToOne(() => User, (user: User) => user.projects)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany(() => VideoAsset, (videoAsset: VideoAsset) => videoAsset.project)
+  videoAssets: VideoAsset[];
+
+  @OneToMany(() => DubJob, (dubjob: DubJob) => dubjob.project)
+  dubJobs: DubJob[];
+}
