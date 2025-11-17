16e4ccbfa5728ed92d3c86ccfca6064bf1452281
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-11-17 15:30:14 +0900

    feat: add VideoAsset entity

---
+++ src/entities/video-asset.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { Project } from './project.entity';
+
+export type lang = 'en' | 'ko';
+
+@Entity('video_assets')
+export class VideoAsset extends BaseEntity {
+  @Column({ name: 'project_id' })
+  projectId: string;
+
+  @Column({ name: 's3_key' })
+  s3Key: string;
+
+  @Column({ name: 'src_lang' })
+  srcLang: lang;
+
+  @Column({ name: 'dst_lang' })
+  dstLang: lang;
+
+  @Column({ type: 'json', name: 'media_meta', nullable: true })
+  mediaMeta: Record<string, any>;
+
+  @ManyToOne(() => Project, (project) => project.videoAssets)
+  @JoinColumn({ name: 'project_id' })
+  project: Project;
+}
