4a96c8cc2d55d3c4b91c673c9151e9e8071aba32
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-11-19 18:47:21 +0900

    chore: enable persistent authorization in Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,12 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   // Public API Spec
   try {
