65330e58266fa8e69aaf777f7edc7b48902356df
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-11-19 18:51:35 +0900

    chore: add TypeOrmModule imports for Project and VideoAsset

--- src/modules/videos/videos.module.ts
+++ src/modules/videos/videos.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
+import { Project } from 'src/entities/project.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideoAsset } from 'src/entities/video-asset.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Project, VideoAsset])],
   controllers: [VideosController],
   providers: [VideosService],
 })
