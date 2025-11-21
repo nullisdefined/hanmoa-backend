05efba9d954da52cba0ee26dbc05da3ae4587ab7
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-11-21 16:02:07 +0900

    feat: import DubJobModule in VideosModule

--- src/modules/videos/videos.module.ts
+++ src/modules/videos/videos.module.ts
@@ -4,9 +4,10 @@ import { VideosController } from './videos.controller';
 import { Project } from 'src/entities/project.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideoAsset } from 'src/entities/video-asset.entity';
+import { DubJobsModule } from '../dub-jobs/dub-jobs.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, VideoAsset])],
+  imports: [TypeOrmModule.forFeature([Project, VideoAsset]), DubJobsModule],
   controllers: [VideosController],
   providers: [VideosService],
 })
